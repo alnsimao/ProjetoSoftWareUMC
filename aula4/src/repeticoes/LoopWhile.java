8a3312b7c978cfc461bf26de1f8094f060b0aba8
bibs <>
2025-03-12 10:09:34 -0300

    Limit 5

--- aula4/src/repeticoes/LoopWhile.java
+++ aula4/src/repeticoes/LoopWhile.java
@@ -8,23 +8,24 @@ public class LoopWhile {
 		Scanner sc = new Scanner(System.in);
 		String loginSalvo = "admin";
 		String senhaSalva = "admin";
+		int tentativa = 0;
 		
 		String login = "";
 		String senha = "";
 		
-		while (true) {
+		do {
 			System.out.println("Digite seu login: ");
 			login = sc.nextLine();
 			System.out.println("Digite sua senha: ");
 			senha = sc.nextLine();
 			
-			if(login .equals(loginSalvo) && senha.equals(senhaSalva)) {
+			if(login.equals(loginSalvo) && senha.equals(senhaSalva)) {
 				System.out.println("Acesso liberado");
 				break;
 			} else {
 				System.out.println("Acesso negado, tente novamente!");
 			}
-		}
+		} while(++tentativa < 5);
 		sc.close();
 
 	}
