ef3fb5dd9453c9ca0b769e01162f04a9b81f54cb
bibs <>
2025-03-12 09:02:28 -0300

    TEste
    testando

--- src/main/LoopDoWhile.java
+++ src/main/LoopDoWhile.java
@@ -1,26 +1,24 @@
 package main;
-
 import java.util.Scanner;
-
 public class LoopDoWhile {
-
 	public static void main(String[] args) {
 		// Jogo Adivinhação
-		
 		Scanner sc = new Scanner(System.in);
 		int maquina = (int)(Math.random()*10);
 		System.out.println(maquina);
 		int usuario;
-		
+
 		do {
 			System.out.println("Informe um número: ");
 			usuario = sc.nextInt();
+			if (usuario < maquina) {
+				System.out.println("Número Menor");
+			} else if (usuario > maquina) {
+				System.out.println("Número Maior");
+			} 
 		} while (usuario != maquina);
-		
 		System.out.println("BOA PAIZÃO");
 			
-		
-		
 		
 		sc.close();
 
