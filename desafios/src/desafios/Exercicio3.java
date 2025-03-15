69988c156c716426305b0f841a6abb0d73715c57
alnsimao <alansimao9a1@gmail.com>
2025-03-15 19:19:36 -0300

    Exercicio 3 FINISHED

---
+++ desafios/src/desafios/Exercicio3.java
@@ -0,0 +1,43 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		//PROGRAMA DE EMPRESTIMO
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+		System.out.println("Insira o valor que você quer receber do empréstimo: ");
+		double valor = sc.nextDouble();
+		double valorTotal = 0;
+		System.out.println("Insira a quantidade de parcelas: ");
+	
+		System.out.println("1- Empréstimo 6 meses: 5% do valor total");
+		System.out.println("2- Empréstimo 12 meses: 10% do valor total");
+		System.out.println("3- Empréstimo 20 meses: 20% do valor total");
+		int escolha = sc.nextInt();
+	
+		
+		switch (escolha) {
+		case 1: 
+			valorTotal = valor + (valor * 0.05);
+			System.out.println("O valor total a ser pago vai ser: " +valorTotal);
+			break;
+		case 2: 
+			 valorTotal = valor + (valor * 0.20);
+			System.out.println("O valor total a ser pago vai ser: " +valorTotal);
+		case 3: 
+			valorTotal = valor + (valor * 0.30);
+			System.out.println("O valor total a ser pago vai ser: " +valorTotal);
+		}
+		
+		
+		
+		sc.close();
+
+	}
+
+}
