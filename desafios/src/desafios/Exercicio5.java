51e66a5de913c807868ceed4238b6ca6b11f6fa4
alnsimao <alansimao9a1@gmail.com>
2025-03-22 15:49:25 -0300

    Exercicio5

--- desafios/src/desafios/Exercicio5.java
+++ desafios/src/desafios/Exercicio5.java
@@ -1,10 +1,40 @@
 package desafios;
 
+import java.util.Scanner;
+
 public class Exercicio5 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // Número Par ou Ímpar
+        Scanner sc = new Scanner(System.in);
+        double numero = 0;
+        double somaPares = 0;
+        double somaImpares = 0;
+        int countPar = 0;
+        int countImp = 0;
 
-	}
+        System.out.println("Coloque um número: ");
+        numero = sc.nextInt();
 
+        while (numero != 0) {
+            if (numero % 2 == 0) {
+                somaPares += numero; 
+                countPar++;
+            } else {
+                somaImpares += numero; 
+                countImp++;
+            }
+
+            System.out.println("Coloque um número (0 para sair): ");
+            numero = sc.nextInt();
+        }
+
+        System.out.println("Soma dos Pares: " + somaPares);
+        System.out.println("Soma dos Ímpares: " + somaImpares);
+        System.out.println("Quantidade Pares: "+countPar);
+        System.out.println("Quantidade Ímpares: "+countImp);
+        
+
+        sc.close();
+    }
 }
